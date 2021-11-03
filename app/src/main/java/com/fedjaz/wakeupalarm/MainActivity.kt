8eadf22de228ef1c991cb72708e1e682c4d99931
fedjaz <fedjaz5@gmail.com>
2021-11-03 19:56:36 +0300

    add: initial project

---
+++ app/src/main/java/com/fedjaz/wakeupalarm/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fedjaz.wakeupalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
